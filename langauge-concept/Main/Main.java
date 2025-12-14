d9888505ea25a44761ca936b7986ca5f5bfa3eb5
nihad1213 <nihad.nemetli@gmail.com>
2025-12-15 00:00:43 +0400

    test

---
+++ langauge-concept/Main/Main.java
@@ -0,0 +1,7 @@
+class Main {
+
+    public static void main(String[] args) {
+        int 1name = 1;
+        System.out.println("This is main function\n");
+    }
+}
---
+++ langauge-concept/comments/Comments.java
@@ -0,0 +1,13 @@
+class Comments {
+
+    public static void main(String[] args) {
+        // Single Line Comment
+        /*
+         * Multi line Comment
+         */
+        /**
+         * JavaDoc
+          */
+        System.out.println("test\n");
+    }
+}
