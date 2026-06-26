91899a724a0ed1f552e3dfa74d4011ff9faa600f
Nihad <nihad.nemetli@gmail.com>
2026-06-26 16:40:06 +0400

    Client class writed

--- github-user-activity/src/main/java/com/activity/GitHubClient.java
+++ github-user-activity/src/main/java/com/activity/GitHubClient.java
@@ -1,4 +1,43 @@
 package com.activity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class GitHubClient {
+
+    public String fetchEvents(String username) {
+        try {
+            URL url = new URL("https://api.github.com/users/" + username + "/events");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("User-Agent", "github-activity-cli");
+
+            int status = conn.getResponseCode();
+
+            if (status == 404) {
+                System.out.println("Error: User \"" + username + "\" not found.");
+                return null;
+            }
+            if (status != 200) {
+                System.out.println("Error: HTTP " + status);
+                return null;
+            }
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            reader.close();
+            return sb.toString();
+        } catch (Exception e) {
+            System.out.println("Error: Unable to connect to GitHub API." + e);
+            return null;
+        }
+    }
 }
--- github-user-activity/src/main/java/com/activity/Main.java
+++ github-user-activity/src/main/java/com/activity/Main.java
@@ -2,10 +2,12 @@ package com.activity;
 
 public class Main {
     static void main(String[] args) {
-        if (args.length > 0 && args[0].equals("github-activity") && args[1].size > 0) {
-
-        } else {
-            System.out.println("No or invalid argument.");
+        if (args.length < 1) {
+            System.out.println("Usage: github-activity <username>");
         }
+
+
+        String username = args[0];
+
     }
 }
