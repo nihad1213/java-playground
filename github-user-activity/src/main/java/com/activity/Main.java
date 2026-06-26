15c6e4cfb44e42c5f099b02a708e84048251b6e0
Nihad <nihad.nemetli@gmail.com>
2026-06-26 16:31:55 +0400

    new: 2 new class created

--- github-user-activity/src/main/java/com/activity/Main.java
+++ github-user-activity/src/main/java/com/activity/Main.java
@@ -2,7 +2,7 @@ package com.activity;
 
 public class Main {
     static void main(String[] args) {
-        if (args.length > 0 && args[0].equals("github-activity")) {
+        if (args.length > 0 && args[0].equals("github-activity") && args[1].size > 0) {
 
         } else {
             System.out.println("No or invalid argument.");
